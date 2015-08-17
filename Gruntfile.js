22e4222878c3217af67ace0af933cdda3d352b35
Jose Antonio Sanchez <jasancheg@gmail.com>
2015-08-17 08:53:57 -0600

    server task comments

--- Gruntfile.js
+++ Gruntfile.js
@@ -403,7 +403,7 @@ module.exports = function (grunt) {
     });
 
 
-    grunt.registerTask('serve', 'Compile then start a connect web server', function (target) {
+    grunt.registerTask('serve', 'Compile then start a connect web server mode for the Electron App', function (target) {
         if (target === 'dist') {
             return grunt.task.run(['build', 'connect:dist:keepalive']);
         }
@@ -419,7 +419,7 @@ module.exports = function (grunt) {
     });
 
     grunt.registerTask('server', 'DEPRECATED TASK. Use the "serve" task instead', function (target) {
-        grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
+        grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server mode of the Electron App.');
         grunt.task.run(['serve:' + target]);
     });
 
