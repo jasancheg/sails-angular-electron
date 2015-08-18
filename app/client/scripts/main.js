c186c020e9c2ec1bb5ab853a1b6beab39a7f7ad3
Jose Antonio Sanchez <jasancheg@gmail.com>
2015-08-18 16:30:16 -0600

    add context menu with toggle dev tools functionality

--- app/client/scripts/main.js
+++ app/client/scripts/main.js
@@ -5,6 +5,27 @@ function runScriptsInRenderedProcess() {
     // adding jquery through of 'require'
     // to know more about what is this, refer to the issue https://github.com/atom/electron/issues/254
     window.$ = window.jQuery = require('jquery');
+
+    var ipc = require('ipc');
+    var remote = require('remote');
+    var Menu = remote.require('menu');
+    var MenuItem = remote.require('menu-item');
+
+    // create a context menu
+    var menu = new Menu();
+
+    menu.append(new MenuItem({ label: 'Toggle Dev Tools', click: function() { ipc.send('toggle-dev-tools'); } }));
+
+    window.addEventListener('contextmenu', function (e) {
+        e.preventDefault();
+        menu.popup(remote.getCurrentWindow());
+    }, false);
+
+    // web contens loaded
+    setTimeout(function () {
+        ipc.send('ready');
+    }, 10);
+
 }
 
 try {
