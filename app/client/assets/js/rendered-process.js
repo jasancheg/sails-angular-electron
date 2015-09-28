9ca14ef9c748a73182d9f8dc0fecd73255b5332c
Jose Antonio Sanchez <j.a.sanchez.gonzalez@accenture.com>
2015-09-28 02:39:57 -0600

    add angular logic Part 2.1

--- app/client/assets/js/rendered-process.js
+++ app/client/assets/js/rendered-process.js
@@ -1,4 +1,57 @@
 'use strict';
+function titleBar(ipc) {
+
+    var ALT = 18;
+
+    window.TitleBar = function(options) {
+        
+        if(!(this instanceof TitleBar)) return new TitleBar(options);
+
+        this._options = options || {};
+
+        var $element = $('#titlebar');
+
+        this.element = $element;
+
+        if(this._options.draggable !== false) $element.addClass('webkit-draggable');
+
+        var self = this,
+            isFullscreen = false,
+            close = process.platform === 'darwin' ? 'titlebar-close' : 'windows-close',
+            minimize = process.platform === 'darwin' ? 'titlebar-minimize' : 'windows-minimize',
+            fullscreen = process.platform === 'darwin' ? 'titlebar-fullscreen' : 'windows-toggle-fullscreen';
+
+        $element.on('click', function(e) {
+            if(e.target.className.indexOf(close) !== -1) {
+                ipc.send('close');
+            } else{ 
+                if(e.target.className.indexOf(minimize) !== -1){
+                    ipc.send('minimize');
+                } else {
+                    if(e.target.className.indexOf(fullscreen) !== -1 && process.platform !== 'darwin') {
+                        ipc.send('maximize');
+                    } else {
+                        if(e.target.className.indexOf(fullscreen) !== -1) ipc.send('fullscreen');
+                    }
+                }
+            }
+        });
+
+        $element.on('dblclick', function(e) {
+            if(e.target === close || e.target === minimize || e.target === fullscreen) return;
+            ipc.send('maximize');
+        });
+
+        $('body').on('keydown', this._onkeydown = function(e) {
+            if(e.keyCode === ALT) $element.addClass('alt');
+        });
+
+        $('body').on('keyup', this._onkeyup = function(e) {
+            if(e.keyCode === ALT) $element.removeClass('alt');
+        });
+
+    };
+}
 
 function runScriptsInRenderedProcess() {
 
@@ -27,6 +80,9 @@ function runScriptsInRenderedProcess() {
     window.dataOs = {
         platform: process.platform
     };
+
+    // activate title var events
+    titleBar(ipc);
 
     // web contens loaded
     setTimeout(function () {
