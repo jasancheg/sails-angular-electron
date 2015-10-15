d5f2a0f570e9920f191a73476b8425a708d429d0
Jose Antonio Sanchez <jasancheg@gmail.com>
2015-10-14 23:45:24 -0600

    add passport Login and passport register strategy

---
+++ app/server/api/services/LocalStrategy.js
@@ -0,0 +1,113 @@
+var passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy,
+    Passwords = require('machinepack-passwords'),
+
+    localStrategy,
+    strategyOptions = {
+        usernameField: 'email'
+    };
+
+function findById(id, fn) {
+    User.findOne(id).exec(function (err, user) {
+        if (err) {
+            return fn(null, null);
+        } else {
+            return fn(null, user);
+        }
+    });
+}
+
+// Passport session setup.
+// To support persistent login sessions, Passport needs to be able to
+// serialize users into and deserialize users out of the session. Typically,
+// this will be as simple as storing the user ID when serializing, and finding
+// the user by ID when deserializing.
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
+});
+ 
+passport.deserializeUser(function (id, done) {
+    findById(id, function (err, user) {
+        done(err, user);
+    });
+});
+
+localStrategy = {
+    login: new LocalStrategy(strategyOptions, function (email, password, done) {
+
+        var searchUser = {
+            email: email
+        };
+
+        User.findOne(searchUser).exec(function (err, user) {
+            if (err) return done(err);
+
+            if (!user) return done(null, false, {
+                message: 'Wrong email/password'
+            });
+
+            Passwords.checkPassword({
+                passwordAttempt: password,
+                encryptedPassword: user.password
+            }).exec({
+                error: function (err) {
+                    return done(err);
+                },
+                incorrect: function () {
+                    return done(null, false, {
+                        message: 'Wrong email/password'
+                    });
+                },
+                success: function () {
+                    return done(null, user, {
+                        message: 'Logged In Successfully'
+                    });
+                }
+            });
+        })
+    }),
+
+    register: new LocalStrategy(strategyOptions, function (email, password, done) {
+
+        var searchUser = {
+            email: email
+        };
+
+        User.findOne(searchUser).exec(function (err, user) {
+            if (err) return done(err);
+
+            if (user) return done(null, false, {
+                message: 'email already exists'
+            });
+
+            Passwords.encryptPassword({
+                password: password,
+                difficulty: 10
+            }).exec({
+                error: function (err) {
+                    return done(err);
+                },
+                success: function (encryptedPassword) {
+
+                    User.create({
+                        email: email,
+                        password: encryptedPassword,
+                        lastLoggedIn: new Date()
+                    }, function (err, newUser) {
+                        if (err) {
+                            return done(err);
+                        }
+                        return done(null, newUser, {
+                            message: 'Account successfully created'
+                        });
+                    });
+                }
+            });
+        });
+    })
+};
+
+passport.use('local-register', localStrategy.register);
+passport.use('local-login', localStrategy.login);
+
+module.exports = localStrategy;
